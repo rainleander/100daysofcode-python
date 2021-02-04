acc23d3ebb305ea170cb4e3d248bef212c1bf410
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-04 10:18:27 +0100

    Update main.py

--- day019/main.py
+++ day019/main.py
@@ -1,1 +1,14 @@
+# Higher Order Functions and Event Listening
+from turtle import Turtle, Screen
 
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
+screen.exitonclick()
