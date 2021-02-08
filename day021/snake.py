a08498497eb53a0351e290082731d315cace6d61
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-08 17:59:53 +0100

    Update snake.py

--- day021/snake.py
+++ day021/snake.py
@@ -6,6 +6,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
 
     def __init__(self):
@@ -15,11 +16,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for turtle_number in range(len(self.segments) - 1, 0, -1):
