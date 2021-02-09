22e2d11ae6095334628eec607e52ace3786e0ff7
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-09 11:05:14 +0100

    Update snake.py

--- day021/snake.py
+++ day021/snake.py
@@ -1,57 +1,27 @@
-from turtle import Turtle
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-LEFT = 180
-RIGHT = 0
+from turtle import Screen
+from paddle import Paddle
 
+screen = Screen()
 
-class Snake:
+screen.bgcolor("black")
+screen.title("PONG!")
+screen.setup(width=800, height=600)
+screen.tracer(0)
 
-    def __init__(self):
-        self.segments = []
-        self.create_snake()
-        self.head = self.segments[0]
+paddle_r = Paddle((350, 0))
+paddle_l = Paddle((-350, 0))
 
-    def create_snake(self):
-        for position in STARTING_POSITIONS:
-            self.add_segment(position)
+screen.listen()
 
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
 
-    def add_segment(self, position):
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        self.segments.append(new_segment)
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
 
+game_is_on = True
 
-    def extend(self):
-        self.add_segment(self.segments[-1].position())
+while game_is_on:
+    screen.update()
 
-    def move(self):
-        for turtle_number in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[turtle_number - 1].xcor()
-            new_y = self.segments[turtle_number - 1].ycor()
-            self.segments[turtle_number].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
-
-    def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
-
-
-    def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-
-
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
-
-
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
+screen.exitonclick()
