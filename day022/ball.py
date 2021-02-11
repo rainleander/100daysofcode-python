e96cfa8b834e0b8f3095609b67d439129f9f7f86
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-11 12:36:45 +0100

    Update ball.py

--- day022/ball.py
+++ day022/ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.goto(0, 0)
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
 
     def move(self):
@@ -21,7 +22,15 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+        self.move_speed *= 0.8
 
 
     def bounce_x(self):
         self.x_move *= -1
+
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
+        self.bounce_y()
