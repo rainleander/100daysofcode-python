d934f1a9066649fdd41b22f7bbb083b765dd3735
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-13 14:48:45 +0100

    Update main.py

--- day023/main.py
+++ day023/main.py
@@ -5,10 +5,34 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.setup(width=600, height=600)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    scoreboard.update_scoreboard()
+    car_manager.create_car()
+    car_manager.drive()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.ycor() == 300:
+        player.next_level()
+        car_manager.speed_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
