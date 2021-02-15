842f79ec989fe357113ab37b90d7ff10eff572cc
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-15 16:41:58 +0100

    Create scoreboard.py

---
+++ day024/better_snake_game/scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Serif", 12, "bold")
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+
+        self.high_score = 0
+        self.hideturtle()
+        self.setposition(0.00, 280.00)
+        self.pencolor("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
