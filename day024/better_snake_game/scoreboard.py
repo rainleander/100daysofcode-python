8a6130dbe6af17144be34dd5b0666e359533d075
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-15 17:08:01 +0100

    Update scoreboard.py

--- day024/better_snake_game/scoreboard.py
+++ day024/better_snake_game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Serif", 12, "bold")
 
@@ -8,8 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.setposition(0.00, 280.00)
         self.pencolor("white")
@@ -26,6 +27,7 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
-
