1722f178f1f389ce84609e5b39c574b0f4d9edb7
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-15 16:41:45 +0100

    Create food.py

---
+++ day024/better_snake_game/food.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(0.25, 0.25)
+        self.penup()
+        self.speed("fastest")
+        self.color("blue")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
