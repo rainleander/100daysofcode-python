a84986fd64e9128faf6dd1bc79164812f51c78a2
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-16 13:06:54 +0100

    Update main.py

--- day024/mail-merge-project/main.py
+++ day024/mail-merge-project/main.py
@@ -1,11 +1,11 @@
-with open("Input/Names/invited_names.txt") as data:
+with open("./Input/Names/invited_names.txt") as data:
     names = [line.strip() for line in data]
 
-with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+with open("./Input/Letters/starting_letter.txt", mode="r") as starting_letter:
     starting_letter_list = starting_letter.readlines()
 
 new_address = []
 for x in range(len(names)):
     new_address = starting_letter_list[0].replace("[name]", names[x])
-    with open(f"Output/ReadyToSend/letter_to_{names[x]}.txt", mode="w") as file:
+    with open(f"./Output/ReadyToSend/letter_to_{names[x]}.txt", mode="w") as file:
         file.write(f"{new_address}\n{starting_letter_list[2]}\n{starting_letter_list[4]}\n{starting_letter_list[6]}")
