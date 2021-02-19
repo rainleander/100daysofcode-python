d42460ef3c99288d4be05a98d09f7ca0488199cf
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-19 08:52:40 +0100

    Update main.py

--- day025/main.py
+++ day025/main.py
@@ -2,12 +2,17 @@
 #     data = [line.strip() for line in weather]
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
