a104ea5c8b03098edfcc507aa9a38fc42fa742fd
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-20 13:09:12 +0100

    Update main.py

--- day025/united-states-game/main.py
+++ day025/united-states-game/main.py
@@ -20,3 +20,35 @@ while len(correct_answers) < 51:
         print(correct_answers)
 
 screen.exitonclick()
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. Naming Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+correct_answers = []
+
+df = pandas.read_csv("50_states.csv")
+state_list = df.state.to_list
+
+
+def write_the_state(answer, x, y):
+    label = turtle.Turtle()
+    label.hideturtle()
+    label.penup()
+    label.goto(x, y)
+    label.write(f"{answer}")
+
+
+while len(correct_answers) != 50:
+    answer_state = screen.textinput(f"{len(correct_answers)}/50 Correct!", "What's another state's name?")
+    titled_answer_state = answer_state.title()
+    if titled_answer_state in state_list():
+        correct_answers.append(titled_answer_state)
+        state = df[df.state == titled_answer_state]
+        write_the_state(titled_answer_state, int(state.x), int(state.y))
+
+screen.exitonclick()
