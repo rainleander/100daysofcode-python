4aa7d06a4ea32ecf0e767b1f661f837ec41719aa
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 18:10:02 +0100

    Update main.py

--- day026/dict-comp-1/main.py
+++ day026/dict-comp-1/main.py
@@ -1,1 +1,8 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
 
+# Write your code below:
+split_sentence = sentence.split()
+result = {x:len(x) for x in split_sentence}
+
+print(result)
