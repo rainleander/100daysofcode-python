c84e0894dcd3281cf22ddea8ad6d9e45df906087
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 18:13:51 +0100

    Update main.py

--- day026/dict-comp-2/main.py
+++ day026/dict-comp-2/main.py
@@ -1,1 +1,15 @@
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+# Write your code 👇 below:
 
+
+
+print(weather_f)
