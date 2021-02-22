791c184fdf462c4c96325546494df8969b38bf49
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 14:48:54 +0100

    Update main.py

--- day026/main.py
+++ day026/main.py
@@ -4,7 +4,14 @@
 numbers = [1, 2, 3]
 new_numbers = [n+1 for n in numbers]
 print(new_numbers)
+# output: [2, 3, 4]
 
 name = "Rain"
 name_list = [letter for letter in name.lower()]
 print(name_list)
+# output: ['r', 'a', 'i', 'n']
+
+single_range = range(1,5)
+double_range = [x*2 for x in single_range]
+print(double_range)
+# output: [2, 4, 6, 8]
