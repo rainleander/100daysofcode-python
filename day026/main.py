54d59895c7b90ec39efbc0c84b90e9732a66e735
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 14:56:28 +0100

    Update main.py

--- day026/main.py
+++ day026/main.py
@@ -2,7 +2,7 @@
 # Create a new list from a previous list
 # new_list = [new_item for item in list]
 numbers = [1, 2, 3]
-new_numbers = [n+1 for n in numbers]
+new_numbers = [n + 1 for n in numbers]
 print(new_numbers)
 # output: [2, 3, 4]
 
@@ -13,7 +13,18 @@ print(name_list)
 # output: ['r', 'a', 'i', 'n']
 
 # challenge: create a new list from a range, where the list items are double the values in a range
-single_range = range(1,5)
-double_range = [x*2 for x in single_range]
+single_range = range(1, 5)
+double_range = [x * 2 for x in single_range]
 print(double_range)
 # output: [2, 4, 6, 8]
+
+# conditional list comprehension
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+# output: ['Alex', 'Beth', 'Dave']
+
+# challenge: create a new list that contains the names longer than 5 characters in ALL CAPS
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
+# output: ['CAROLINE', 'ELEANOR', 'FREDDIE']
