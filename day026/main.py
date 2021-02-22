91250124b1354d8fc9c8e285e585b6efc11be7bb
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 14:50:27 +0100

    Update main.py

--- day026/main.py
+++ day026/main.py
@@ -6,11 +6,13 @@ new_numbers = [n+1 for n in numbers]
 print(new_numbers)
 # output: [2, 3, 4]
 
+# challenge: what will happen when list comprehension is applied to a string?
 name = "Rain"
 name_list = [letter for letter in name.lower()]
 print(name_list)
 # output: ['r', 'a', 'i', 'n']
 
+# challenge: create a new list from a range, where the list items are double the values in a range
 single_range = range(1,5)
 double_range = [x*2 for x in single_range]
 print(double_range)
