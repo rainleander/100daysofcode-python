3cc71963566539f9aab5d6e00605f51fd2c29e4a
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 15:55:42 +0100

    Update main.py

--- day026/data-overlap/main.py
+++ day026/data-overlap/main.py
@@ -1,3 +1,10 @@
+file1 = open("file1.txt", "r")
+file1_list = file1.read().splitlines()
+
+file2 = open("file2.txt", "r")
+file2_list = file2.read().splitlines()
+
+result = [int(x) for x in file1_list if x in file2_list]
 
 # Write your code above 👆
 
