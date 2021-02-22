da09374f5cde0a5eab5cd966ca8bc90e72c0e006
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 09:30:42 +0100

    Update main.py

--- day025/united-states-game/main.py
+++ day025/united-states-game/main.py
@@ -9,30 +9,8 @@ turtle.shape(image)
 
 correct_answers = []
 
-data = pandas.read_csv("50_states.csv")
-state_list = data.state.to_list
-
-while len(correct_answers) < 51:
-    answer_state = screen.textinput("Guess the State", "What's another state's name?")
-    answer_state.title()
-    if answer_state in state_list():
-        correct_answers.append(answer_state)
-        print(correct_answers)
-
-screen.exitonclick()
-import turtle
-import pandas
-
-screen = turtle.Screen()
-screen.title("U.S. Naming Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
-
-correct_answers = []
-
 df = pandas.read_csv("50_states.csv")
-state_list = df.state.to_list
+state_list = df.state.to_list()
 
 
 def write_the_state(answer, x, y):
@@ -43,12 +21,19 @@ def write_the_state(answer, x, y):
     label.write(f"{answer}")
 
 
-while len(correct_answers) != 50:
+while len(correct_answers) < 51:
     answer_state = screen.textinput(f"{len(correct_answers)}/50 Correct!", "What's another state's name?")
     titled_answer_state = answer_state.title()
-    if titled_answer_state in state_list():
+
+    if titled_answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in correct_answers:
+                missing_states.append(state)
+        missing_data = pandas.DataFrame(missing_states)
+        missing_data.to_csv("states_to_learn.csv")
+        break
+    if titled_answer_state in state_list:
         correct_answers.append(titled_answer_state)
         state = df[df.state == titled_answer_state]
         write_the_state(titled_answer_state, int(state.x), int(state.y))
-
-screen.exitonclick()
