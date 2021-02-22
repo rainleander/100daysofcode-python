4dc7fca8097943e5b3834497dabf6cb95bc7cf29
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-22 14:40:16 +0100

    Update main.py

--- day026/main.py
+++ day026/main.py
@@ -1,1 +1,6 @@
-
+# List Comprehension
+# Create a new list from a previous list
+# new_list = [new_item for item in list]
+numbers = [1, 2, 3]
+new_numbers = [n+1 for n in numbers]
+print(new_numbers)
