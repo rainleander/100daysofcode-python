892d458e485350daddc062f20cfacc71e40eb0d0
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-23 09:33:20 +0100

    Update main.py

--- day026/main.py
+++ day026/main.py
@@ -42,3 +42,25 @@ print(student_scores)
 
 passed_students = {student:score for (student, score) in student_scores.items() if score >= 60}
 print(passed_students)
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    print(row.score)
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
