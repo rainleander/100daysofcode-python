abfddff41e4b403e5098d8e2283e3a7d09281cd6
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-23 10:50:43 +0100

    Update main.py

--- day027/main.py
+++ day027/main.py
@@ -1,1 +1,14 @@
+import tkinter
 
+window = tkinter.Tk()
+window.title("My First TKinter GUI Program")
+window.minsize(width=500, height=300)
+
+# How to create a Label
+my_label = tkinter.Label(text="I Am A Label", font=("Arial", 24, "bold"))
+
+# TKinter pack method https://docs.python.org/3/library/tkinter.html#the-packer
+my_label.pack()
+
+# while loop that keeps the window alive so that the user can interact
+window.mainloop()
