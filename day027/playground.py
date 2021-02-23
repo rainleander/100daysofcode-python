5b6918eb4f67a329711064b311c4811b962f7ef9
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-23 17:05:37 +0100

    Update playground.py

--- day027/playground.py
+++ day027/playground.py
@@ -11,3 +11,31 @@ def add(*args):
 
 numbers_list = (1, 2, 3, 45, 700)
 print(add(numbers_list))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(n, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.year = kw.get("year")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+print(my_car.model)
