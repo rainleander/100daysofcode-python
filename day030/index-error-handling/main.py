ba8e50a71cb25210092060c9077bc96dec29b48b
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-26 19:51:45 +0100

    Create main.py

---
+++ day030/index-error-handling/main.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Pear", "Orange"]
+
+#TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
+
+
+make_pie(4)
