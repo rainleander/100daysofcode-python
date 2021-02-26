ff941b246239e3aa11776c4780fa2f41a677d998
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-26 20:02:36 +0100

    Create main.py

---
+++ day030/key-error-handling/main.py
@@ -0,0 +1,16 @@
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    total_likes = total_likes + post['Likes']
+
+
+print(total_likes)
