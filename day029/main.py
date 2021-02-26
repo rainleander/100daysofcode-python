3623dd5f990569872043c64bf494225e5b251fa4
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-26 16:47:06 +0100

    Update main.py

--- day029/main.py
+++ day029/main.py
@@ -1,8 +1,28 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_input.get()
+    username = email_username_input.get()
+    password = password_input.get()
+
+    if website == "" or password == "":
+        messagebox.showinfo(title="Oops!", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=
+            f"These are the details entered:\nEmail: {username}\nPassword: {password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", mode="a") as file:
+                file.write(f"{website} | {username} | {password}\n")
+                website_input.delete(0, END)
+                website_input.focus()
+                password_input.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -20,12 +40,14 @@ website_label.grid(column=0, row=1)
 
 website_input = Entry(width=35)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
 
 email_username_label = Label(text="Username:")
 email_username_label.grid(column=0, row=2)
 
 email_username_input = Entry(width=35)
 email_username_input.grid(column=1, row=2, columnspan=2)
+email_username_input.insert(0, "angela@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
@@ -36,8 +58,7 @@ password_input.grid(column=1, row=3)
 password_button = Button(text="Generate Password", width=10)
 password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=33)
+add_button = Button(text="Add", width=33, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
-
