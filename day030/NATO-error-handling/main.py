9565fcecbace1e479790a1a947708df604f5e065
K Rain Leander <rainleander@users.noreply.github.com>
2021-02-27 13:16:33 +0100

    Update main.py

--- day030/NATO-error-handling/main.py
+++ day030/NATO-error-handling/main.py
@@ -6,10 +6,16 @@ code_dict = {}
 for (index, row) in data.iterrows():
     code_dict[row.letter] = row.code
 
-# TODO: catch the keyerror when a user enters a character that is not in the dictionary
-# TODO: provide feedback to the user when an illegal word is entered
-# TODO: continue prompting the user to enter another word until they enter a valid word
+# DONE: catch the keyerror when a user enters a character that is not in the dictionary
+# DONE: provide feedback to the user when an illegal word is entered
+# DONE: continue prompting the user to enter another word until they enter a valid word
+invalid_word = True
 
-user_input = input("Enter a word: ").upper()
-list_of_code = [code_dict[letter] for letter in user_input]
-print(list_of_code)
+while invalid_word:
+    try:
+        user_input = input("Enter a word: ").upper()
+        list_of_code = [code_dict[letter] for letter in user_input]
+        print(list_of_code)
+        invalid_word = False
+    except KeyError:
+        print("Sorry - only letters in the word, please.")
