daec619b0bf79f2838ec1940eebab7dafae66256
K Rain Leander <rainleander@users.noreply.github.com>
2021-03-09 06:22:07 +0100

    Update main.py

--- day035/main.py
+++ day035/main.py
@@ -1,16 +1,21 @@
+import os
 import requests
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 parameters = {
     "lat": 53.173180,
     "lon": 6.602950,
     "exclude": "current,minutely,daily,alerts",
-    "appid": "19e7f36aef01e2d3728ed9d2e0e3f630",
+    "appid": "## ----- $SECRET ----- ##",
 }
 
-account_sid = "ACafc484d50575b604f88a750b1b1323e5"
-auth_token = "771aa7dbd1f2946e2f6e1ba09fe6eea3"
-client = Client(account_sid, auth_token)
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+account_sid = "## ----- $SECRET ----- ##"
+auth_token = "## ----- $SECRET ----- ##"
+client = Client(account_sid, auth_token, http_client=proxy_client)
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
@@ -21,8 +26,8 @@ weather_data = response.json()["hourly"]
 def send_rain_alert():
     message = client.messages.create(
         body=" It's going to rain today. Bring an umbrella.️",
-        from_='+12512501815',
-        to='+31618547893'
+        from_='## ----- $SECRET ----- ##',
+        to='## ----- $SECRET ----- ##'
     )
 
     print(message.status)
