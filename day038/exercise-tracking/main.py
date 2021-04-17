cdc842d8e95e314314ab544b33f75ae40e0e0b64
K Rain Leander <rainleander@users.noreply.github.com>
2021-04-17 11:36:32 +0200

    Update main.py

--- day038/exercise-tracking/main.py
+++ day038/exercise-tracking/main.py
@@ -5,10 +5,14 @@ APP_ID = "031d8a77"
 API_KEY = "e526412b17bfa722fc3ad487a8b5d0f5"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheety_endpoint = ""
-headers = {
+sheety_endpoint = "https://api.sheety.co/a72fb3c79b3dabb89545d0ff6200f208/workoutTracking/workouts"
+
+exercise_headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
+}
+sheety_headers = {
+    "Content-Type": "application/json",
 }
 
 query = input("Tell me which exercises you did: ")
@@ -21,8 +25,24 @@ exercise_data = {
     "age": 44,
 }
 
-response = requests.post(url=exercise_endpoint, json=exercise_data, headers=headers)
-print(response.text)
+exercise_response = requests.post(url=exercise_endpoint, json=exercise_data, headers=exercise_headers)
+result = exercise_response.json()
 
 today = datetime.now()
 date = today.strftime("%d/%m/%Y")
+time = today.strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+sheety_response = requests.post(url=sheety_endpoint, json=sheet_inputs)
+
+print(sheety_response.text)
