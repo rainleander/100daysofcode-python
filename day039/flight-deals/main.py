3afb04ab1fbd066b286fc62750b27cf2d11b745f
K Rain Leander <rainleander@users.noreply.github.com>
2021-04-20 09:30:21 +0200

    Update main.py

--- day039/flight-deals/main.py
+++ day039/flight-deals/main.py
@@ -1,3 +1,15 @@
+from data_manager import DataManager
+from pprint import pprint
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+# pprint(sheet_data)
 
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
-# to achieve the program requirements.
+if sheet_data[0]["iataCode"] == "":
+    from flight_search import FlightSearch
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    print(f"sheet_data:\n {sheet_data}")
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
