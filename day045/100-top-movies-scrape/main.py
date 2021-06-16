0f172f9dd5f007a97c7f63e5f95e8b329fca2c95
K Rain Leander <rainleander@users.noreply.github.com>
2021-06-16 12:01:30 +0200

    Update main.py

--- day045/100-top-movies-scrape/main.py
+++ day045/100-top-movies-scrape/main.py
@@ -1,1 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+empire_site = response.text
+
+soup = BeautifulSoup(empire_site, "html.parser")
+
+all_movies = soup.find_all(name="titleText")
+
+# movies_list = []
+#
+# movies = soup.find_all(name="h3")
+#
+# for x in movies:
+#     movie = x.getText()
+#     movies_list.append(movie)
+
+print(all_movies)
